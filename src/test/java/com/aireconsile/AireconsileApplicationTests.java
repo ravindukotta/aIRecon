08b90db4ade114302fe61660639a5331cad20c11
ravindu <kottahachchiravindu@gmail.com>
2026-08-30 17:46:52 +0800

    Initial Spring Batch project scaffold

---
+++ src/test/java/com/aireconsile/AireconsileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aireconsile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AireconsileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
